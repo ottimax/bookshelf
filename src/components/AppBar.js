8b04723be4199e9ab7188fee325cab8ccadef34b
ottimax <pepsy__97@hotmail.it>
2020-09-26 23:48:01 +0200

    Creazione AppBar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
+// IMPORT CSS
+import './AppBar.css'
+
+
+// IMPORT MATERIAL-UI COMPONENT AND ICON
+import { Button, InputBase, Toolbar, Typography } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search';
+
+
 export default function AppBar() {
     return (
         <div>
-            
+            <Toolbar className="toolbar">
+                <div className="toolbar-content">
+                    <SearchIcon className="search-icon" fontSize="large" />
+                    <InputBase className="input" placeholder="Cerca Libro" />
+                    <Button className="input-button" variant="contained" color="rgb(24, 64, 87)" href="">
+                        Cerca
+                    </Button>
+                </div>
+            </Toolbar>
         </div>
     )
 }
